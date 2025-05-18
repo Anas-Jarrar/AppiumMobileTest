6fadfe30586929d9819cb1113893f987999af0ba
Anas-Jarrar <anasjarrar944@gmail.com>
2025-05-18 03:49:53 +0300

    Last Update

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,9 +1,16 @@
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
+import java.util.Date;
 
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
@@ -15,40 +22,50 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class MyTest {
-	DesiredCapabilities caps=new DesiredCapabilities();
+	DesiredCapabilities caps = new DesiredCapabilities();
 	AndroidDriver driver;
-	
+
 	@BeforeMethod
-	public void  BeforeEachTest() throws MalformedURLException {
-		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);		
+	public void BeforeEachTest() throws MalformedURLException, URISyntaxException {
+//		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);		
+		driver = new AndroidDriver(new java.net.URI("http://127.0.0.1:4723/wd/hub").toURL(), caps);
+
 	}
+
 	@BeforeTest
 	public void MySetup() {
 		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "abc");
-		File MyApplication=new File ("src/MyApplication/calculator.apk");
-		caps.setCapability(MobileCapabilityType.APP, MyApplication.getAbsolutePath());		
+		File MyApplication = new File("src/MyApplication/calculator.apk");
+		caps.setCapability(MobileCapabilityType.APP, MyApplication.getAbsolutePath());
 	}
-	
-	@Test(enabled=false)
-	public void MyTest () throws MalformedURLException {
+
+	@Test(priority = 1, enabled = false)
+	public void MyTest() throws MalformedURLException {
 		driver.findElement(By.id("com.google.android.calculator:id/digit_5")).click();
 		driver.findElement(By.id("com.google.android.calculator:id/op_mul")).click();
 		driver.findElement(By.id("com.google.android.calculator:id/digit_9")).click();
-		String result=driver.findElement(By.id("com.google.android.calculator:id/result_preview")).getText();
+		String result = driver.findElement(By.id("com.google.android.calculator:id/result_preview")).getText();
 		Assert.assertEquals(result, "45");
 //		Assert.assertEquals(Integer.parseInt(result), 45);
 	}
-	@Test
-	public void ClickOnlyOnTheNumbers ()  {
-		List<WebElement> allButtons=driver.findElements(By.className("android.widget.ImageButton"));
-		for(int i=0;i<allButtons.size();i++) {
-			
-			if(allButtons.get(i).getDomAttribute("resource-id").contains("digit")) {
+
+	@Test(priority = 2)
+	public void ClickOnlyOnTheNumbers() throws IOException {
+
+		List<WebElement> allButtons = driver.findElements(By.className("android.widget.ImageButton"));
+		for (int i = 0; i < allButtons.size(); i++) {
+
+			if (allButtons.get(i).getDomAttribute("resource-id").contains("digit")) {
 				allButtons.get(i).click();
 			}
-			
+
 		}
-	
+		Date date = new Date();
+		String FileName=date.toString().replace(":", "-");
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File fileScreenShoot = ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(fileScreenShoot, new File("src/ScreenShoots/"+FileName+".jpg"));
+
 	}
 }
